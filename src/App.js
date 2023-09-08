bedbf4b5c28fff3713d20b8037d7e84303668506
JoseanGomez1 <joseangomez123@gmail.com>
2023-09-08 09:24:32 -0700

    some addtions

--- src/App.js
+++ src/App.js
@@ -5,24 +5,40 @@ import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import DogList from './DogList';
-import DogDropdown from './DogDropdown'; // Import the DogDropdown component
+import DogDropdown from './DogDropdown'; // drop down men import
+import Backgroundimage from './components/Backgroundimage'; // Import the Backgroundimage component
 
 
 function App() {
 
   return (
-    <div className="App">
-       <h1>Dog Information App</h1>
-      <DogDropdown /> {/* Render the DogDropdown component */}
-      <DogList /> {/* Render the DogList component */}
-      <Header />
-      <Routes>
-      <Route exact path="/" element={<Home />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
-}
+      <div className="App min-h-screen bg-gray-100">
+        <Backgroundimage /> {/* Use the Backgroundimage component as the background */}
+        <div className="relative z-10 p-4">
+          <h1 className="text-3xl font-semibold mb-4">Dog Information App</h1>
+          <div className="mb-4">
+            <DogDropdown />
+          </div>
+          <div className="bg-white p-4 rounded-lg shadow-lg">
+            <DogList />
+          </div>
+          {}
+        </div>
+      </div>
+    );
+  }
+//     <div className="App">
+//        <h1>Dog Information App</h1>
+//       <DogDropdown /> {/* Render the DogDropdown component */}
+//       <DogList /> {/* Render the DogList component */}
+//       <Header />
+//       <Routes>
+//       <Route exact path="/" element={<Home />} />
+//       </Routes>
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
 
---
+++ src/components/Backgroundimage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Backgroundimage = () => {
+  return (
+    <div
+      className="fixed top-0 left-0 w-full h-full bg-cover bg-center z-0"
+      style={{
+        backgroundImage: `url("https://cdn.pixabay.com/photo/2015/09/15/18/17/paw-print-941498_1280.jpg")`,
+      }}
+    >
+      {}
+    </div>
+  );
+};
+
+export default Backgroundimage;
